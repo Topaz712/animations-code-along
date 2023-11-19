43a3c03972ef47bcf6b1a3c29ef8436edcfdec91
Topaz712 <tzam712@gmail.com>
2023-11-18 19:55:27 -0600

    advanced transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,47 @@ import { Component } from '@angular/core';
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))// <=> does the same; means back and forth use same animate function
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
-    onDelete(item) {
-      this.list.splice(this.list.indexOf(item), 1);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
